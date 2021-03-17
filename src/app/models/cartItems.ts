ed60edc3f0a20baaf8ec4b3c89de63b060b31e79
Zafer Çalışkan <40596204+zafercaliskan@users.noreply.github.com>
2021-03-18 01:14:28 +0300

    17 March end of day (#2)

---
+++ src/app/models/cartItems.ts
@@ -0,0 +1,4 @@
+import { CartItem } from './cartItem';
+//public olduğu içinb PascalCase yazılır.
+//Array birden fazla sepete ürün eklenebilir diye.
+export const CartItems: CartItem[] = [];
--- src/app/services/cart.service.spec.ts
+++ src/app/services/cart.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ProductService } from './product.service';
+import { CartService } from './cart.service';
 
-describe('ProductService', () => {
-  let service: ProductService;
+describe('CartService', () => {
+  let service: CartService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductService);
+    service = TestBed.inject(CartService);
   });
 
   it('should be created', () => {
